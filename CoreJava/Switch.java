b9bba77d510ac518a913e91801a09472a5affbc3
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-22 11:42:12 +0530

    CoreJava

--- CoreJava/Switch.java
+++ CoreJava/Switch.java
@@ -2,6 +2,26 @@ class  Switch
 {
 	public static void main(String[] args) 
 	{
+
+		System.out.println("Example One ");
+		int num = 2;
+		switch(num)
+		{
+			case 1:
+				System.out.println("Case-1");
+				break;
+			case 2:
+				System.out.println("Case-2");
+				break;
+			case 3:
+				System.out.println("Case-3");
+				break;
+			default:
+				System.out.println("Default");
+				break;
+		}
+		
+
 		switch("Pavan")
 		{
 			case "Pavan":
