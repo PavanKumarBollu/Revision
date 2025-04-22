2ab8cdab9a269d0343a36788ff44e59c1abcfdd1
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-22 11:38:41 +0530

    CoreJava

---
+++ CoreJava/Switch.java
@@ -0,0 +1,15 @@
+class  Switch
+{
+	public static void main(String[] args) 
+	{
+		switch("Pavan")
+		{
+			case "Pavan":
+				System.out.println("Pavan");
+				break;
+			default:
+				System.out.println("Default");
+				break;
+		}
+	}
+}
