20bf38248d0cb6aad70e9893fdfeeb14a805b62d
PavanKumarB <bollupavankumar12@gmail.com>
2025-05-02 22:53:43 +0530

    InnerClassesInJava

--- CoreJava/InnerClass.java
+++ CoreJava/InnerClass.java
@@ -1,7 +1,19 @@
+class OuterClass
+{
+	class InnerClass
+	{
+		public void childDis()
+		{
+		}
+	}
+}
+
+
+
 class InnerClass 
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		System.out.println("Reguler Inner Class");
 	}
 }
