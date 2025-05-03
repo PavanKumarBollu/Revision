20f7a4e66b0351a4d975706e0e96e6a9fda3ffa3
PavanKumarB <bollupavankumar12@gmail.com>
2025-05-03 12:44:26 +0530

    ArraysAndArraysMethods

--- CoreJava/ArraysMethods.java
+++ CoreJava/ArraysMethods.java
@@ -72,9 +72,9 @@ class ArraysMethods
 		System.out.println("Arrays.setAll(array, lamdaExpresion) Method ");
 
 		int arr7 = new int[7];
-		Arrays.setAll(arr7, i->{i * 2});
+		Arrays.setAll(arr7, i->i * 2);
 
-		System.out.println("After Arrays.setAll() method Execution :  " Arrays.toString(arr7));
+		System.out.println("After Arrays.setAll() method Execution :  " + Arrays.toString(arr7));
 		
 		
 	}
