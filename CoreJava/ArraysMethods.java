314b12abd85f020992ceac91300e0aa568d5afce
PavanKumarB <bollupavankumar12@gmail.com>
2025-05-03 12:03:49 +0530

    ArraysAndArraysMethods

--- CoreJava/ArraysMethods.java
+++ CoreJava/ArraysMethods.java
@@ -2,6 +2,10 @@ class ArraysMethods
 {
 	public static void main(String[] args) 
 	{
+		System.out.println("Array Before Sorting : " + Arrays.toString(arr1));
 		int[] arr1 = new int[]{5,4,3,2,1};
+		System.out.println("Array After Sorting : " + Arrays.toString(arr1));
+		System.out.println(" ");
+		System.out.println(" ");
 	}
 }
